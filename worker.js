6489f70db31285998deb376f93ad7a9c6e180be6
h-lyf <57041119+h-lyf@users.noreply.github.com>
2024-06-10 10:36:29 +0800

    Add files via upload

--- worker.js
+++ worker.js
@@ -16,8 +16,8 @@ const protect_keylist = [
   "password",
 ]
 
-let index_html = "https://crazypeace.github.io/Url-Shorten-Worker/" + config.theme + "/index.html"
-let result_html = "https://crazypeace.github.io/Url-Shorten-Worker/" + config.theme + "/result.html"
+let index_html = "https://h-lyf.github.io/Url-Shorten-Worker/" + config.theme + "/index.html"
+let result_html = "https://h-lyf.github.io/Url-Shorten-Worker/" + config.theme + "/result.html"
 
 const html404 = `<!DOCTYPE html>
   <html>
@@ -294,7 +294,7 @@ async function handleRequest(request) {
   // 如果path为空, 即直接访问本worker
   // If visit this worker directly (no path)
   if (!path) {
-    return Response.redirect("https://zelikk.blogspot.com/search/label/Url-Shorten-Worker", 302)
+    return Response.redirect("https://rin.omt.us.kg", 302)
     /* new Response(html404, {
       headers: response_header,
       status: 404
